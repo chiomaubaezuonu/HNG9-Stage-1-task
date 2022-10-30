3b443ff3850b35f512c54fdf766a1824b436ff31
Dancyangelo102 <chiomaubaezuonu102@gmail.com>
2022-10-30 01:15:02 +0100

    Updated media query

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,9 +4,9 @@ const Footer = () => {
   return (
     <div className="footer">
         <div className = "footer-links">
-          <img src="./images/Zuri-logo.png" alt="logo"/>
-          <img src="./images/footer-text.png" alt="text"/>
-          <img src="./images/I4G.png" alt="I4G"/>
+          <img src="./images/Zuri-logo.png" className="contact" alt="logo"/>
+          <img src="./images/footer-text.png" className="contact" alt="text"/>
+          <img src="./images/I4G.png" className="contact" alt="I4G"/>
         </div>
     </div>
   )
--- src/Components/Links.js
+++ src/Components/Links.js
@@ -4,25 +4,25 @@ import "../Components/variables.scss";
 const Links = () => {
   return (
     <div className="link-div">
-      <a href="https://twitter.com/ChiomaPresh1029" id="twitter">
+      <a href="https://twitter.com/ChiomaPresh1029" id="twitter" className="links">
         <button>ChiomaPresh1029</button>
       </a>
-      <a href="https://training.zuri.team/" id="btn__zuri">
+      <a href="https://training.zuri.team/" id="btn__zuri" className="links">
         <button>Zuri Team</button>
       </a>
-      <a href="http://books.zuri.team" id="books">
+      <a href="http://books.zuri.team" id="books" className="links">
         <button>Zuri Books</button>
       </a>
       <a
         href="https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>"
-        id="book__python"
+        id="book__python" className="links"
       >
         <button>Phython Books</button>
       </a>
-      <a href="https://background.zuri.team" id="pitch">
+      <a href="https://background.zuri.team" id="pitch" className="links">
         <button>Background Check for Coders</button>
       </a>
-      <a href="https://books.zuri.team/design-rules" id="book__design">
+      <a href="https://books.zuri.team/design-rules" id="book__design" className="links">
         <button>Design Books</button>
       </a>
       <div className="social-media">
